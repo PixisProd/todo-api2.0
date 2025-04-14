f7789295c1a98d5749f78b52376c6d39aa70ac1a
Jahongir Hodjaniyazov <jahongirhodjaniyazov@Jahongirs-MacBook-Air.local>
2025-04-14 11:33:25 +0500

    Prepared application for Jinja2 templating

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
 from contextlib import asynccontextmanager
 
 from src import router
 from src.database import create_tables, delete_tables
+
+
+templates = Jinja2Templates(directory="templates")
 
 
 @asynccontextmanager
@@ -15,5 +19,6 @@ app = FastAPI(
     title="ToDo 2.0",
     lifespan=lifespan
 )
+
 
 app.include_router(router)
